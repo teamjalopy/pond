1ec17514ddcab296d33bdba880953c387b89ebd0
Paul Herz <phrz@me.com>
2016-03-19 15:24:45 -0500

    Fixed template error

--- src/routes.php
+++ src/routes.php
@@ -5,5 +5,5 @@ $app->get('/', function ($request, $response, $args) {
 
     $this->logger->info("Pond '/' route");
 
-    return $this->renderer->render($response, 'landing.html', $args);
+    return $this->renderer->render($response, 'index.phtml', $args);
 });
