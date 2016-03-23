1b4f126fd4c0628294616c7ebbfa73da5a59856d
Paul Herz <phrz@me.com>
2016-03-23 17:29:15 -0500

    Revert "Merge branch 'master' into dev"
    This reverts commit b44b6f67a25d042dc1fc2e82918509aa0fc96c31, reversing
    changes made to 7a54f56d32090389f0561ee47de5c5d3a2888734.

--- src/settings.php
+++ src/settings.php
@@ -13,5 +13,18 @@ return [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
         ],
+
+        // Eloquent settings
+        'eloquent' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'database' => 'pond',
+            'username' => 'pond',
+            'password' => 'familiar-history-procession-remark',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+            'prefix' => '',
+        ],
+
     ],
 ];
