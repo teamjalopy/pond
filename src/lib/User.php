d078f12dfe2095845b670d82fd675c6464e916ff
Vianka Barboza <vbarboza@smu.edu>
2016-03-23 21:39:16 -0500

    Both get and post work well. Working on taking in arguments next

--- src/lib/User.php
+++ src/lib/User.php
@@ -3,5 +3,6 @@
 
     class User extends \Illuminate\Database\Eloquent\Model {
         public $primaryKey = 'user_id';
+        protected $fillable = array('id', 'username', 'name', 'type', 'password', 'salt', 'created_at', 'updated_at');
     }
 ?>
--- src/routes.php
+++ src/routes.php
@@ -9,10 +9,29 @@ $app->get('/', function ($request, $response, $args) {
 });
 
 
-$app->get('/users', function($req, $res, $args) {
+
+/*$app->get('/users', function($req, $res, $args) {
     $users = \Pond\User::all();
     $stat = new \Pond\StatusContainer($users);
     $stat->message("Here are all the users");
     return $res->withJson($stat);
 });
+ */
+
+$app->get('/users/{user_id}', function($req, $res, $args) {
+    $users = \Pond\User::find(2);
+    $stat = new \Pond\StatusContainer($users);
+    $stat->message("Here is user_id 1");
+    return $res->withJson($stat);
+});
+
+$app->post('/users', function($req, $res, $args) {
+    $users = \Pond\User::create(array('user_id' => 2, 'username' =>'kBeth', 'name' => "Kimberly Beth", 'type' => 'STUDENT', 'password' => '21346', 'salt' => 'peanut', 'created_at' => '2008-11-11 13:23:44', 'updated_at' => '2008-11-11 11:12:01' ));
+    $stat = new \Pond\StatusContainer($users);
+    $stat->message("Here is a newly created user");
+    return $res->withJson($stat);
+});
+
+
+
  
