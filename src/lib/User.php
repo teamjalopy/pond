5a3ba280472e1df1760ad15c383ec297b8b6a5d7
Vianka Barboza <vbarboza@smu.edu>
2016-03-24 12:07:27 -0500

    Changing way I post information for the User

--- src/lib/User.php
+++ src/lib/User.php
@@ -3,6 +3,5 @@
 
     class User extends \Illuminate\Database\Eloquent\Model {
         public $primaryKey = 'user_id';
-        protected $fillable = array('id', 'username', 'name', 'type', 'password', 'salt', 'created_at', 'updated_at');
     }
 ?>
