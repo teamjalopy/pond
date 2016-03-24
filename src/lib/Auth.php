46317eda023b36f2f530c99c03743f75b51e6428
Paul Herz <phrz@me.com>
2016-03-23 21:21:23 -0500

    Auth::loginHandler null value error fix
    • Fixed null value error in loginHandler
    • Added securely ambiguous error message to all login
    validation/authentication failures

--- src/lib/Auth.php
+++ src/lib/Auth.php
@@ -9,45 +9,41 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Auth {
 
-    static public function loginHandler(Request $req, Response $res) {
+    static public function loginHandler(Request $req, Response $res): Response {
         $form = $req->getParsedBody();
 
         // Username format validation
         try {
-            \Pond\Validate::get('username')->check( $form['username'] );
+            \Pond\Validate::get('username')->check( $form['username'] ?? null );
         } catch(ValidationException $e) {
-            $stat = new \Pond\StatusContainer();
-            $stat->error('InvalidUsername');
-            $stat->message('Please enter a valid username.');
-
-            $res = $res->withStatus(400);
-            return $res->withJson($stat);
+            return badAuthResponse($res);
         }
 
         // Password format validation
         try {
-            \Pond\Validate::get('password')->check( $form['password'] );
+            \Pond\Validate::get('password')->check( $form['password'] ?? null );
         } catch(ValidationException $e) {
-            $stat = new \Pond\StatusContainer();
-            $stat->error('InvalidPassword');
-            $stat->message('Please enter a valid password.');
-
-            $res = $res->withStatus(400); // Bad Request
-            return $res->withJson($stat);
+            return badAuthResponse($res);
         }
 
         // Try to get the requested User or throw an exception
         try {
             $reqUser = User::where('username', $form['username'])->firstOrFail();
         } catch(ModelNotFoundException $e) {
-            $stat = new \Pond\StatusContainer();
-            $stat->error('BadAuth');
-            $stat->message('Check your username and password.');
-
-            $res = $res->withStatus(401); // Unauthorized
-            return $res->withJson($stat);
+            return badAuthResponse($res);
         }
+
+        // Check the password
         return $res;
     } // loginHandler
+
+    static function badAuthResponse(Response $res): Response {
+        $stat = new \Pond\StatusContainer();
+        $stat->error('BadAuth');
+        $stat->message('Check your username and password.');
+
+        $res = $res->withStatus(401); // Unauthorized
+        return $res->withJson($stat);
+    }
 
 } // Auth
