eff8a5af48a0176d132f2441d174eaba07e38487
Paul Herz <phrz@me.com>
2016-03-23 21:51:22 -0500

    Password authentication

--- src/lib/Auth.php
+++ src/lib/Auth.php
@@ -12,30 +12,44 @@ class Auth {
     static public function loginHandler(Request $req, Response $res): Response {
         $form = $req->getParsedBody();
 
+        if(! $token = self::authenticate($form['username'],$form['password']) ) {
+            return self::badAuthResponse($res);
+        }
+
+
+        return $res;
+    } // loginHandler
+
+    private static function authenticate($user, $password) {
         // Username format validation
         try {
             \Pond\Validate::get('username')->check( $form['username'] ?? null );
         } catch(ValidationException $e) {
-            return badAuthResponse($res);
+            return null;
         }
 
         // Password format validation
         try {
             \Pond\Validate::get('password')->check( $form['password'] ?? null );
         } catch(ValidationException $e) {
-            return badAuthResponse($res);
+            return null;
         }
 
         // Try to get the requested User or throw an exception
         try {
             $reqUser = User::where('username', $form['username'])->firstOrFail();
         } catch(ModelNotFoundException $e) {
-            return badAuthResponse($res);
+            return null;
         }
 
         // Check the password
-        return $res;
-    } // loginHandler
+        $knownP = $reqUser->password;
+        $givenP = new Crypto($form['password'], $reqUser->salt);
+
+        if(!Crypto::compare($knownP,$givenP)) {
+            return null;
+        }
+    }
 
     static function badAuthResponse(Response $res): Response {
         $stat = new \Pond\StatusContainer();
@@ -43,6 +57,15 @@ class Auth {
         $stat->message('Check your username and password.');
 
         $res = $res->withStatus(401); // Unauthorized
+        return $res->withJson($stat);
+    }
+
+    static function tokenResponse(Response $res, Token $t): Response {
+        $stat = new \Pond\StatusContainer( ['token' => (string)$t] );
+        $stat->success();
+        $stat->message('Successfully logged in.');
+
+        $res = $res->withStatus(201); // Created
         return $res->withJson($stat);
     }
 
