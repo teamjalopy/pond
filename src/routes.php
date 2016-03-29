92a1d75b270df4280da5b2dcb93aa178f9c77a50
Vianka Barboza <vbarboza@smu.edu>
2016-03-28 22:31:43 -0500

    Working Registration API!

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,17 @@
 <?php
 // Routes
 
+$app->post('/api/auth', function ($req, $res, $args) {
+    $auth = new \Pond\Auth($this);
+    return $auth->loginHandler($req, $res);
+});
+$app->get('/api/auth/{user_id}', function ($req, $res) {
+    $auth = new \Pond\Auth($this);
+    $uid = $req->getAttribute('user_id');
+    $isAuth = $auth->isRequestAuthorized($req,$uid);
+    return $isAuth ? $res->withStatus(200) : $res->withStatus(401);
+});
+
 $app->get('/api/users/{user_id}', function($req, $res, $args) {
     $users = Pond\User::find($args['user_id']);
     $stat = new \Pond\StatusContainer($users);
