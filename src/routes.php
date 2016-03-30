9fcb93e787c42bb605750162164912d2a4e23b5b
Jeffrey Tang <jktang@smu.edu>
2016-03-30 18:19:03 -0500

    Work in Progress on Routes
    Filled in post and get for /api/lessons

--- src/routes.php
+++ src/routes.php
@@ -14,38 +14,49 @@ $app->get('/api/auth/{user_id}', function ($req, $res) {
 });
 
 $app->get('/api/lessons/{lesson_id}', function($req, $res, $args) {
-    
+
 });
 
 $app->put('/api/lessons/{lesson_id}', function($req, $res, $args) {
-    
+
 });
 
 $app->delete('/api/lessons/{lesson_id}', function($req, $res, $args) {
-    
+
 });
 
 $app->get('/api/lessons', function($req, $res, $args) {
-    $db = new \Pond\Lesson($this);
-	$array = [];
-	$jsonResponse = $response->withHeader("Content-type","application/json");
-	
-	foreach($db->query('SELECT * FROM lessons') as $row) {
-    	array_push($array, [
-			"lesson_id" => $row['lesson_id'],
-			"lesson_name" => $row['lesson_name'],
-			"creator_id" => $row['creator_id'],
-			"published" => ($row['published'] == 1)
-		]);
-    }
-	
-	return $jsonResponse->write( json_encode($array) );
-	
+  $lessonObj = [];
+
+  // $jsonResponse = $res->withHeader("Content-type","application/json");
+  foreach($lessons as $lesson){
+    array_push($lessonObj,[
+      "id" => $lesson->lesson_id,
+      "name" =>$lesson->lesson_name,
+      "creator" => $lesson->creator_id
+    ]);
+  }
+  /*array_push($array,[
+    "id" => "52",
+    "name" => "things",
+    "creator" => "him"
+  ]);*/
+
+  // return $jsonResponse->write(json_encode($array));
+  return $res->withJson($lessonObj);
 });
 
 $app->post('/api/lessons', function($req, $res, $args) {
-    $db = new \Pond\Lesson($this);
-    $creator = $req->getAttribute('creator_id'); 
-	$lesson = $req->getAttribute('lesson_name');
-	$db->query("INSERT INTO lessons (creator_id,lesson_name) VALUES ('$creator','$lesson');");
+  $lesson = new \Pond\Lesson();
+  $form = $req->getParsedBody();
+  $lesson->lesson_id = @$form['lesson_id'];
+  $lesson->creator_id = @$form['creator_id'];
+  $lesson->lesson_name = @$form['lesson_name'];
+  $lesson->published = @$form['published'];
+  $lesson->save();
+	$array = array("lesson"=>$lesson_name,"creator"=>$creator);
+	$jsonResponse = $res->withHeader("Content-type","application/json");
+	return $jsonResponse->write(json_encode($array));
+
+
 });
