2024098c3e20aa3d6f8b7b8340a09567b77c056c
Paul Herz <phrz@me.com>
2016-03-31 00:57:35 -0500

    Added step form and validation

--- public/app/HomeView/HomeController.js
+++ public/app/HomeView/HomeController.js
@@ -16,4 +16,26 @@ angular.module('pond.HomeView', ['ngRoute'])
 
     $scope.topLinkText = 'Log in';
     $scope.topLink = '#/log-in';
+
+    // Multi-step form animation
+    $scope.shiftBy = 350; // amount to increment shift
+    $scope.shiftAmount = 0; // counter for shift pixels
+    $scope.shiftCSS = ""; // resulting CSS of shift
+    $scope.shiftElements = 2;
+    $scope.maxShift = $scope.shiftBy * $scope.shiftElements;
+
+    $scope.nextStep = function() { // increment shifter
+        $scope.increment($scope.shiftBy * -1);
+    };
+
+    $scope.prevStep = function() { // decrement shifter
+        $scope.increment($scope.shiftBy);
+    };
+
+    $scope.increment = function(amount) {
+        $scope.shiftAmount += amount;
+        $scope.shiftAmount = $scope.shiftAmount % $scope.maxShift;
+        var css = {transform:'translateX('+$scope.shiftAmount+'px)'};
+        $scope.shiftCSS = css
+    }
 }]);
--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var app =
 angular.module('pond', [
     'ngRoute',
     'ngAnimate',
@@ -15,3 +16,33 @@ angular.module('pond', [
 .value('settings',{
     'baseURI': 'http://pond.dev/'
 });
+
+// Prevent view/partial caching
+app.run(function($rootScope, $templateCache) {
+   $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
+});
+
+// Compare To validator
+// [CITE] http://plnkr.co/edit/FipgiTUaaymm5Mk6HIfn?p=preview
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+}
+
+app.directive("compareTo", compareTo);
