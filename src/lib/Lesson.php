d17f548be2843e361e21f0a763b116ec9035ed04
Jeffrey Tang <jktang@smu.edu>
2016-03-30 19:32:07 -0500

    Working POST /api/lessons

--- src/lib/Lesson.php
+++ src/lib/Lesson.php
@@ -4,8 +4,6 @@
     class Lesson extends \Illuminate\Database\Eloquent\Model {
         public $primaryKey = 'lesson_id';
 
-        public function creator(){
-        return $this->hasOne('Pond\User');
-        }
+        
     }
 ?>
--- src/routes.php
+++ src/routes.php
@@ -28,13 +28,9 @@ $app->delete('/api/lessons/{lesson_id}', function($req, $res, $args) {
 $app->get('/api/lessons', function($req, $res, $args) {
   $lessonObj = [];
   $lessons = \Pond\Lesson::all();
- 
+
   foreach($lessons as $lesson){
-    array_push($lessonObj,[
-      "lesson_id" => $lesson->lesson_id,
-      "lesson_name" =>$lesson->lesson_name,
-      "creator_id" => $lesson->creator_id
-    ]);
+    array_push($lessonObj, $lesson->toArray());
   }
 
   return $res->withJson($lessonObj);
@@ -43,14 +39,10 @@ $app->get('/api/lessons', function($req, $res, $args) {
 $app->post('/api/lessons', function($req, $res, $args) {
   $lesson = new \Pond\Lesson();
   $form = $req->getParsedBody();
-  //$lesson->lesson_id = @$form['lesson_id'];
+
   $lesson->creator_id = @$form['creator_id'];
   $lesson->lesson_name = @$form['lesson_name'];
-//  $lesson->published = @$form['published'];
   $lesson->save();
-	$array = array("lesson"=>$lesson_name,"creator"=>$creator);
-	$jsonResponse = $res->withHeader("Content-type","application/json");
-	return $jsonResponse->write(json_encode($array));
 
 
 });
