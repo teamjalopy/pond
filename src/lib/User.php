a273aff7d326660c13eb5a7b7df9bfd11375118d
Paul Herz <phrz@me.com>
2016-04-05 08:35:30 -0500

    Obfuscated password in User model

--- src/lib/User.php
+++ src/lib/User.php
@@ -11,5 +11,21 @@
         public function lessons() {
             return $this->hasMany('Pond\Lesson','creator_id');
         }
+
+        protected $casts = [
+            'validated' => 'boolean',
+        ];
+
+        protected $hidden = ['password','salt','created_at','updated_at','validation_token','type'];
+
+        public function getIsTeacherAttribute() {
+            return $this->attributes['type'] == 'TEACHER';
+        }
+
+        public function getIsStudentAttribute() {
+            return $this->attributes['type'] == 'STUDENT';
+        }
+
+        protected $appends = ['is_teacher', 'is_student'];
     }
 ?>
