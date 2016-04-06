29448dfea74a3735624af997013c4819fd4d1295
ahavens <ahavens@smu.edu>
2016-04-06 17:33:01 -0500

    js file added to studentview
    js file filled, needs to be fixed

--- public/app/StudentDashView/StudentDashController.js
+++ public/app/StudentDashView/StudentDashController.js
@@ -1,1 +1,15 @@
 //Student Dash JS
+'use strict';
+
+angular.module('pond.StudentDashView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/student-dash', {
+        templateUrl: 'app/common/DashTemplate.html'
+        controller: 'StudentDashController'
+    });
+}])
+
+.controller('StudentDashController', ['$scope', '$http', '$location', 'settings', function($scope, $http, $location, settings) {
+    $scope.pagePartial = "/app/StudentDashView/StudentDashPartial.html";
+}]);
