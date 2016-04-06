7230df8c47184e22ec36d473bb08a1adbb7867bd
ahavens <ahavens@smu.edu>
2016-04-06 18:01:50 -0500

    Getting student name
    attempting to make the JS get the student name

--- public/app/StudentDashView/StudentDashController.js
+++ public/app/StudentDashView/StudentDashController.js
@@ -7,9 +7,24 @@ angular.module('pond.StudentDashView', ['ngRoute'])
     $routeProvider.when('/student-dash', {
         templateUrl: 'app/common/DashTemplate.html'
         controller: 'StudentDashController'
+        student: ''
     });
 }])
 
-.controller('StudentDashController', ['$scope', '$http', '$location', 'settings', function($scope, $http, $location, settings) {
+.controller('StudentDashController', ['$scope', '$http', '$location', 'settings', 
+	function($scope, $http, $location, settings) {
     $scope.pagePartial = "/app/StudentDashView/StudentDashPartial.html";
+
+    $http({
+        'method': 'POST',
+        'url': settings.baseURI + 'api/users/1',
+        'headers': { 'Content-Type' : 'application/json'}, // explicitly provide the content type
+        // pass the data object (the Content-Type above will mean it gets implicitly encoded as JSON)
+        'data': name
+    })
+    .then( function(response){
+    	$scope.student = reponse.data.name
+    }
+    );
+
 }]);
