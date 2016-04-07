90aed635ec923a5ed230c77050f4c4e84f8d617d
dmattka <dmattka@gmail.com>
2016-04-06 20:47:15 -0500

    Controller JS

--- public/app/TeacherDashView/TeacherDashController.js
+++ public/app/TeacherDashView/TeacherDashController.js
@@ -1,1 +1,16 @@
-//Teacher Dash JS
+'use strict';
+
+angular.module('myApp.TeacherView', ['$scope','settings'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/second-view', {
+        templateUrl: 'app/common/DashTemplate.html',
+        controller: 'TeacherController'
+    });
+}])
+
+.controller('TeacherDashController', ['$scope', 'settings'],
+function($scope, settings) {
+    $scope.viewName = 'TeacherDashView';
+    $scope.pagePartial = "/app/TeacherDashView/TeacherDashPartial.html";
+}]);
