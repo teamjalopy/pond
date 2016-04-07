16b4040709ccaabd11558b76843136019d084a5f
Jeffrey Tang <jktang@smu.edu>
2016-04-06 21:37:41 -0500

    Add POST endpoint for password reset

--- src/routes.php
+++ src/routes.php
@@ -14,3 +14,7 @@ $app->any('/api/users[/{user_id}]', '\Pond\UserController');
 
 // Lesson Endpoints
 $app->any('/api/lessons[/{lesson_id}]', '\Pond\LessonController');
+
+$app->post('/users/{user_id}/reset_password/{reset_token}',function($req,$res,$args)){
+    
+});
