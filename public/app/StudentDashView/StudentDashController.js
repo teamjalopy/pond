a888b7d54b37bb18b895150bf32599360113a65f
Paul Herz <phrz@me.com>
2016-04-07 00:28:38 -0500

    Lesson Cards
    - Lesson table replaced with cards in Student dash.
    - Added dummy Lesson model array.
    - Added course-card CSS.

--- public/app/StudentDashView/StudentDashController.js
+++ public/app/StudentDashView/StudentDashController.js
@@ -10,7 +10,7 @@ angular.module('pond.StudentDashView', ['ngRoute'])
     });
 }])
 
-.controller('StudentDashController', ['$scope', '$http', '$location', '$cookies', 'settings', 
+.controller('StudentDashController', ['$scope', '$http', '$location', '$cookies', 'settings',
 function($scope, $http, $location, $cookies, settings) {
     $scope.pagePartial = '/app/StudentDashView/StudentDashPartial.html';
 
@@ -19,13 +19,12 @@ function($scope, $http, $location, $cookies, settings) {
     $http({
         'method': 'GET',
         'url': settings.baseURI + 'api/users/me',
-        'headers': { 
+        'headers': {
         	'Content-Type' : 'application/json',
         	'Authorization' : 'Bearer ' + $cookies.get('token')
         }, // explicitly provide the content type
         // pass the data object (the Content-Type above will mean it gets implicitly encoded as JSON)
-    })
-    .then( 
+    }).then(
     		function successCallback(response) {
     			//get the user data name
                 $scope.user = response.data.data;
@@ -35,13 +34,39 @@ function($scope, $http, $location, $cookies, settings) {
                     $scope.username = $scope.user.email;
                 }
                 console.log($scope.username);
-                
+
             },
             function errorCallback(response) {
                 console.log('Getting username unsuccessful')
             }
-    
     );
+
+    $scope.lessons = [
+        {
+            lesson_id:   1,
+            creator_id:  1,
+            lesson_name: "Lesson One",
+            published: true,
+            created_at: "2016-04-04 19:23:34",
+            updated_at: "2016-04-05 14:10:40"
+        },
+        {
+            lesson_id:   2,
+            creator_id:  1,
+            lesson_name: "Lesson Two",
+            published: true,
+            created_at: "2016-04-04 19:23:34",
+            updated_at: "2016-04-05 14:10:40"
+        },
+        {
+            lesson_id:   3,
+            creator_id:  1,
+            lesson_name: "Lesson Three",
+            published: true,
+            created_at: "2016-04-04 19:23:34",
+            updated_at: "2016-04-05 14:10:40"
+        }
+    ];
 
     //logout test function
     $scope.logOut = function() {
