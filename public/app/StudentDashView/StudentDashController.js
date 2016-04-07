2e8fb73d0aab0559d75318a1b40d43f700172592
ahavens <ahavens@smu.edu>
2016-04-06 19:16:32 -0500

    trying to fix error message

--- public/app/StudentDashView/StudentDashController.js
+++ public/app/StudentDashView/StudentDashController.js
@@ -5,13 +5,13 @@ angular.module('pond.StudentDashView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/student-dash', {
-        templateUrl: 'app/common/DashTemplate.html'
+        templateUrl: 'app/common/DashTemplate.html',
         controller: 'StudentDashController'
-     });
+    });
 }])
 
 .controller('StudentDashController', ['$scope', '$http', '$location', '$cookies', 'settings', 
-	function($scope, $http, $location, $cookies, settings) {
+function($scope, $http, $location, $cookies, settings) {
     $scope.pagePartial = "/app/StudentDashView/StudentDashPartial.html";
 
     $scope.username = '';
