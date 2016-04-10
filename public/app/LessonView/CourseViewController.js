28ca6f297cb93e7b346c965853f350edb3655ec7
thecross6 <arturo2499872@gmail.com>
2016-04-09 19:54:09 -0500

    Created Course Page Mockup
    Added dependency to index.
    Created title, description and three lessons (video, quiz and article).

--- public/app/LessonView/CourseViewController.js
+++ public/app/LessonView/CourseViewController.js
@@ -3,13 +3,13 @@
 
 angular.module('pond.CourseView', ['ngRoute'])
 
-.config(['routeProvider', function($routeProvider) {
-    $routeProvider.when('/course-view', {
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/course-view' , {
         templateUrl: 'app/common/DashTemplate.html',
         controller: 'CourseViewController'
     });
 }])
 
 .controller('CourseViewController', ['$scope', '$http', '$location', '$cookies', 'settings', function($scope, $http, $location, $cookies, settings) {
-    $scope.pagePartial = '/app/StudentDashView/CourseViewPartial.html';
+    $scope.pagePartial = '/app/LessonView/CourseViewPartial.html';
 }]);
