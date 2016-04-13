75104f317f8971e042c5c59f7bc65e325ce00e93
Paul Herz <phrz@me.com>
2016-04-13 18:46:32 -0500

    Polymorphic Quiz/Module models
    - Polymorphic Module-Content relation
    - Quiz model
    - Question model

--- src/lib/Module.php
+++ src/lib/Module.php
@@ -8,4 +8,19 @@
             return $this->belongsTo('Pond\Lesson');
         }
 
+        function content() {
+            return $this->morphTo('contentable');
+        }
+
+        function getTypeAttribute() {
+            return $this->attributes['contentable_type'];
+        }
+
+        function getContentAttribute() {
+            return $this->content()->first()->get();
+        }
+
+        protected $hidden = ['contentable_id', 'contentable_type'];
+        protected $appends = ['type','content'];
+
     }
