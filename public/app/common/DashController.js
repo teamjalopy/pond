b7a0d8844a5d60c7064f4d6a04ed5387e9f11cf2
Paul Herz <phrz@me.com>
2016-04-12 23:32:41 -0500

    Few more navbar changes

--- public/app/common/DashController.js
+++ public/app/common/DashController.js
@@ -4,4 +4,14 @@ angular.module('pond.DashController', [])
 .controller('DashController', function($scope){
     $scope.baseController = "DashController";
     $scope.navCollapsed = true;
+
+    $scope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            console.log("Route change. Collapsing nav.")
+            $scope.navCollapsed = true;
+            // Forcing $scope to update
+            // [CITE] http://nathanleclaire.com/blog/2014/01/31/banging-your-head-against-an-angularjs-issue-try-this/
+            $scope.apply();
+        }
+    });
 });
